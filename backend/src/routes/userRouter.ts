bfaefdffc2ad76455e66ffb0f854233dea6899b4
eclipse01-oe <biomedicalengineer.oe@gmail.com>
2026-05-07 15:25:55 -0400

    creatuser update

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -3,8 +3,9 @@ import { verifyUser } from "../middleware/verifyUserRoute";
 import * as userController from "../controllers/user.controller";
 
 const UserRouter = Router();
-UserRouter.use(verifyUser);
+UserRouter.use(verifyUser)
 
+UserRouter.post('/', userController.createUser);
 UserRouter.get('/', userController.getUsers);
 UserRouter.get('/:id', userController.getUserById);
 UserRouter.put('/:id', userController.updateUser);
