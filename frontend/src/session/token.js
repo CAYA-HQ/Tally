7f406c23b2ca5f7741c437456bf0d4874b47741c
TOLU-MICH <michealtolu2003@gmail.com>
2026-05-09 08:34:38 +0100

    feat: connect login and register pages to auth API

--- frontend/src/session/token.js
+++ frontend/src/session/token.js
@@ -1,7 +1,10 @@
 let accessToken = null;
 
-export const setAccessToken = (token) => {
+export const setAccessToken = (token, user) => {
   accessToken = token;
+  if (user?.id) localStorage.setItem("userId", user.id);
 };
 
 export const getAccessToken = () => accessToken;
+
+export const getUserId = () => localStorage.getItem("userId");
